caecf390b805670311e970065db0399718b6d94e
krch <krch@Ubuntu-22.04.myguest.virtualbox.org>
2023-03-06 19:42:51 +0300

    Add 5th task

--- Exercises/5-range-odd.js
+++ Exercises/5-range-odd.js
@@ -1,5 +1,9 @@
 'use strict';
 
-const rangeOdd = null;
+const rangeOdd = (start, end) => {
+  const result = [];
+  for (let i = start; i <= end; i++) result.push(i);
+  return result.filter((x) => x % 2 !== 0);
+};
 
 module.exports = { rangeOdd };
