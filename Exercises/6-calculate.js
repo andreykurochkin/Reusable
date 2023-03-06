5cb68cf55b0f07a7d1a5a02fba9b1d5a9118f170
krch <krch@Ubuntu-22.04.myguest.virtualbox.org>
2023-03-06 20:16:08 +0300

    Add 6th task

--- Exercises/6-calculate.js
+++ Exercises/6-calculate.js
@@ -1,11 +1,18 @@
 'use strict';
 
-const square = null;
+const pow = (base, power) => base ** power;
+const square = (a) => pow(a, 2);
 
-const cube = null;
+const cube = (a) => pow(a, 3);
 
-const average = null;
+const average = (a, b) => 0.5 * (a + b);
 
-const calculate = null;
+const calculate = () => {
+  const result = [];
+  for (let i = 0; i < 10; i++) {
+    result.push(average(square(i) + cube(i));
+  }
+  return result;
+};
 
 module.exports = { square, cube, average, calculate };
